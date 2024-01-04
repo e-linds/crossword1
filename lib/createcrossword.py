550aaa333f17f300bb2199cb791d61443bd7b908
Emmi Lindsey <emmilindsey@gmail.com>
2024-01-04 10:14:13 -0700

    some more crossword edits...no new functionality yet

--- lib/createcrossword.py
+++ lib/createcrossword.py
@@ -25,103 +25,99 @@ with Session(engine) as session:
 
         word_len = len(newword)
         print(line_full * word_len + box_end) 
-        # print(line_empty * word_len + box_end)
         print_word_horiz(newword)
-        # print(line_empty * word_len + box_end)
         print(line_full * word_len + box_end) 
 
 
 
-    def add_vert_word(newword, index):
+    def add_vert_word(newword, index = None):
             
-            spaces = (' ' * (index * 6))
+            if index:
+                spaces = (' ' * (index * 6))
+            else:
+                spaces = ""
 
             for each in list(newword):
                 print(spaces + line_full + box_end)
-                # print(line_empty + box_end)
                 print(spaces + "+" + (' ' * 2) +(f"{each.upper()}") + (" " * 2) + "+")
-                # print(line_empty + box_end)
-                # print(line_full + box_end)
+               
 
 
     word_list = []
-    grand_array = []
 
+    ui1 = input("What word would you like to add? ")
+    firstword = ((ui1).strip()).lower()
 
-    # word1 = input("What word would you like to add to the crossword? ")
-    # word2 = input("What word would you like to add to the crossword? ")
-
-
-    # word_list.append(list(word1))
-    # word_list.append(list(word2))
-
-    # temporary function until I get word table up and running
-    def add_word(input):
-         word_list.append(list(input))
-
-    # add_word("hello")
-    # add_word("goodbye")
-    # add_word("yellow")
-    # add_word("nugget")
-    word1 = "yellow"
-    word2 = "noodle"
-    add_word(word1)
-    add_word(word2)
-
-
- 
-
-    count = 2
-    matches = []
-    for each in list(word2):
-        count -= 1
-        if count >= 0:
-            for letter in word_list[count]:
-                if word_list[count] != list(word2):
-                    if each == letter:   
-                        match = [count, word_list[count].index(letter)]
-                        #this means match will be [index of matching word, index of matching letter within word]
-                        matches.append(match)
-
-    selected_match = random.choice(matches)
-    match_word = word_list[selected_match[0]]
-    match_letter = match_word[selected_match[1]]
-
-    list_new_word = list(word2)
-    
-   
-
-    # def split_word():
-    split_index = list_new_word.index(match_letter)
-    first_half = list_new_word[0:split_index]
-    second_half = list_new_word[split_index+1: len(list_new_word)+1]
-
-
-    # split_word()
-
-
-
-    #[index of matching word: currently we assume it is across and that we are adding a down]
-    #[index of matching letter within word - this will dictate how many spaces we need to add before the word is printed]
-
-    add_vert_word(first_half, selected_match[1])
-    add_horiz_word(match_word)
-    add_vert_word(second_half, selected_match[1])
+    word_list.append(list(firstword))
     
 
+    add_horiz_word(firstword)
+
+    while True:
+
+        uinext = input("What word would you like to add? ")
+
+        uiword = ((uinext).strip()).lower()
+        word_list.append(list(uiword))
+
+
+        count = len(word_list) - 1
+        matches = []
+        for each in list(uiword):
+            count = count - 1
+            if count >= 0:
+                for letter in word_list[count]:
+                    if word_list[count] != list(uiword):
+                        if each == letter:  
+                            match = [count, word_list[count].index(letter)]
+                            #this means match will be [index of matching word, index of matching letter within word]
+                            matches.append(match)
+
+     
+
+        selected_match = random.choice(matches)
+        match_word = word_list[selected_match[0]]
+        match_letter = match_word[selected_match[1]]
+
+      
+        #[index of matching word within wordlist]
+        #[index of matching letter within word - this will dictate how many spaces we need to add before the word is printed]
+
+        # add vertical word to existing horizontal
+        def display_two_words(vertword, horizword, letter):
+
+            split_index = vertword.index(letter)
+            first_half = vertword[0:split_index]
+            second_half = vertword[split_index+1: len(vertword)+1]
+
+            add_vert_word(first_half, horizword.index(letter))
+            add_horiz_word(horizword)
+            add_vert_word(second_half, horizword.index(letter))
+
+
+        # if new word is an even number, display horizontally
+        if (word_list.index(list(uiword)) % 2) == 0:
+            display_two_words(match_word, uiword, match_letter)
+        #if new word is an odd number, display vertically
+        else:
+            display_two_words(uiword, match_word, match_letter)
+
+
+        # we'll assume that the first word was added horizonally and now the third word is too
+        def display_three_words(newword, oldword, letter):
+
+            add_vert_word(first_half, horizword.index(letter))
+            add_horiz_word(horizword)
+            add_vert_word(second_half, horizword.index(letter))
+            
 
 
 
         
         
-             
-
-    # print(word_list)
+    
 
 
-
-    # add_horiz_word(word1)
-    # add_vert_word(word2)
 
 
 
