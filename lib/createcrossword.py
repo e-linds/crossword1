3813634133240a27eacba3b396180177fac64af0
Emmi Lindsey <emmilindsey@gmail.com>
2024-01-04 08:24:55 -0700

    working crossword functionality (for two words)git add .!

--- lib/createcrossword.py
+++ lib/createcrossword.py
@@ -1,5 +1,6 @@
 from models import *
 from sqlalchemy.orm import Session
+import random
 
 with Session(engine) as session:
 
@@ -31,16 +32,14 @@ with Session(engine) as session:
 
 
 
-    def add_vert_word(newword, relatedword, spaces):
+    def add_vert_word(newword, index):
             
-
-            
-
+            spaces = (' ' * (index * 6))
 
             for each in list(newword):
-                print(line_full + box_end)
+                print(spaces + line_full + box_end)
                 # print(line_empty + box_end)
-                print("+" + (' ' * 2) +(f"{each.upper()}") + (" " * 2) + "+")
+                print(spaces + "+" + (' ' * 2) +(f"{each.upper()}") + (" " * 2) + "+")
                 # print(line_empty + box_end)
                 # print(line_full + box_end)
 
@@ -60,21 +59,60 @@ with Session(engine) as session:
     def add_word(input):
          word_list.append(list(input))
 
-    add_word("hello")
-    add_word("goodbye")
-    add_word("yellow")
-    add_word("nugget")
-    noodle = "noodle"
-    add_word(noodle)
+    # add_word("hello")
+    # add_word("goodbye")
+    # add_word("yellow")
+    # add_word("nugget")
+    word1 = "yellow"
+    word2 = "noodle"
+    add_word(word1)
+    add_word(word2)
 
-    count = 5
-    for each in list(noodle):
+
+ 
+
+    count = 2
+    matches = []
+    for each in list(word2):
         count -= 1
-        for letter in word_list[count]:
-             if each == letter:
-                  print(letter)
-                  print(count)
-                  print(word_list[count].index(letter))
+        if count >= 0:
+            for letter in word_list[count]:
+                if word_list[count] != list(word2):
+                    if each == letter:   
+                        match = [count, word_list[count].index(letter)]
+                        #this means match will be [index of matching word, index of matching letter within word]
+                        matches.append(match)
+
+    selected_match = random.choice(matches)
+    match_word = word_list[selected_match[0]]
+    match_letter = match_word[selected_match[1]]
+
+    list_new_word = list(word2)
+    
+   
+
+    # def split_word():
+    split_index = list_new_word.index(match_letter)
+    first_half = list_new_word[0:split_index]
+    second_half = list_new_word[split_index+1: len(list_new_word)+1]
+
+
+    # split_word()
+
+
+
+    #[index of matching word: currently we assume it is across and that we are adding a down]
+    #[index of matching letter within word - this will dictate how many spaces we need to add before the word is printed]
+
+    add_vert_word(first_half, selected_match[1])
+    add_horiz_word(match_word)
+    add_vert_word(second_half, selected_match[1])
+    
+
+
+
+
+        
         
              
 
